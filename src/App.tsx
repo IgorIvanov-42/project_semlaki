e80fdfde200d3fa6b767e9b5d748b6c671b23ec1
Svetalina <svetalina9@gmail.com>
2025-03-16 20:06:16 +0100

    React Redux

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import GlobalStyles from './styles/GlobalStyles'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      Migrants
+    </BrowserRouter>
+  )
+}
+
+export default App
