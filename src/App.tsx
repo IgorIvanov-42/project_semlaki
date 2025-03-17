cc0d8bb0fbba4c6b4a1605942f029f236ea47ba5
Svetalina <svetalina9@gmail.com>
2025-03-17 10:24:25 +0100

    FE-0 add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import GlobalStyles from './styles/GlobalStyles'
+import Home from 'pages/Home/Home'
+
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyles />
+      <Home />
       Migrants
     </BrowserRouter>
   )
